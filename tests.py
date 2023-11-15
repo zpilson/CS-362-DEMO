c1b8ffc16804e5db53ffc722afb40da97989983b
Zachary C Pilson <107872358+zpilson@users.noreply.github.com>
2023-11-14 20:59:49 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,12 +1,4 @@
 import unittest
-import task
 
-class TestCase(unittest.TestCase):
-
-    def test1(self):
-        expected = "Hello World"
-        self.assertEqual(task.my_func(), expected)
-
-
-  if __name__ == '__main__':
+if __name__ == '__main__':
     unittest.main()
